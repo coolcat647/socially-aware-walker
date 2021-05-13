0c3894ea1ac8ea0ce4f95587aa9335dd61b3b0ba
coolcat647 <b10302017@mail.ntust.edu.tw>
2021-05-13 22:51:36 +0800

    add pushing_action_simulation_node

--- catkin_ws/src/sensors/dynpick_driver/src/force2cmd_node.py
+++ catkin_ws/src/sensors/dynpick_driver/src/force2cmd_node.py
@@ -13,30 +13,28 @@ from geometry_msgs.msg import WrenchStamped, Vector3, Twist
 from nav_msgs.msg import Odometry
 import message_filters
 
-MIN_ENABLE_FORCE = 5.0
-MIN_ENABLE_TORQUE = 2.0
+MIN_ENABLE_FORCE = 6.0
+MIN_ENABLE_TORQUE = 0.8
 
 MAX_LINEAR_VELOCITY = 0.6
-MAX_ANGULAR_VELOCITY = 1.4
+MAX_ANGULAR_VELOCITY = 1.0
 
-MASS = 22.771158961  * 0.1  #10.0 # 30.0
-MOMENT_OF_INERTIA = 22.468527211 * 0.1 # 1.3 # 2.61
-DAMPING_XY = 245.568732467  * 0.1 # 40.0 # 40.0 
-DAMPING_THETA = 245.093435376 * 0.1 # 8.0
-
-DESIRE_V = 0.2
-DESIRE_OMEGA = 0.0
+MASS = 30.0
+MOMENT_OF_INERTIA = 10.0
+DAMPING_XY = 45.0 
+DAMPING_THETA = 20
+CONSTANT_FRACTION_XY = 1e-3
+CONSTANT_FRACTION_THETA = 1e-3
 
 class Force2CmdNode(object):
     def __init__(self):
         self.last_time = None
-        self.last_linear_velocity = 0
-        self.last_angular_velocity = 0
+        self.last_v = 0
+        self.last_w = 0
 
-        self.pub_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-        # self.sub_force = rospy.Subscriber("/force_filtered", WrenchStamped, self.force_cb, queue_size=1)
-        sub_force = message_filters.Subscriber("force_filtered", WrenchStamped)
-        sub_odom = message_filters.Subscriber("wheel_odom", Odometry)
+        self.pub_vel = rospy.Publisher('/walker/cmd_vel', Twist, queue_size=1)
+        sub_force = message_filters.Subscriber("/walker/force_filtered", WrenchStamped)
+        sub_odom = message_filters.Subscriber("/walker/wheel_odom", Odometry)
         ts = message_filters.ApproximateTimeSynchronizer([sub_force, sub_odom], 10, 0.1, allow_headerless=True)
         ts.registerCallback(self.callback)
 
@@ -46,36 +44,36 @@ class Force2CmdNode(object):
     def callback(self, force_msg, odom_msg):
         if self.last_time is None:
             self.last_time = rospy.Time().now()
+            self.last_v = 0
+            self.last_w = 0
             return
 
-        force_y = force_msg.wrench.force.y if np.abs(force_msg.wrench.force.y) > MIN_ENABLE_FORCE else 0.0
+        force_y = (force_msg.wrench.force.y ) if np.abs(force_msg.wrench.force.y) > MIN_ENABLE_FORCE else 0.0
         torque_z = force_msg.wrench.torque.z if np.abs(force_msg.wrench.torque.z) > MIN_ENABLE_TORQUE else 0.0
+
+        total_mass = MASS + (-force_msg.wrench.force.z / 9.8) 
 
         now_time = rospy.Time().now()
         dt = (now_time - self.last_time).to_sec()
         
         # Calculate target velocity by human force
-        # linear_velocity = (force_y / MASS * dt + self.last_linear_velocity) / (1 + DAMPING_XY / MASS * dt)
-        # angular_velocity = (torque_z / MOMENT_OF_INERTIA * dt + self.last_angular_velocity) / (1 + DAMPING_THETA / MOMENT_OF_INERTIA * dt)
-        
-        linear_velocity = (force_y / 15.145 * dt + self.last_linear_velocity) / (15.145 + (-151.03) * dt)
-        angular_velocity = (torque_z / 1.1801 * dt + self.last_angular_velocity) / (15.145 + (-7.975) * dt)
-        
-        # linear_velocity = self.last_linear_velocity * 0.9087 * 0.7 + force_y * 0.0028 * 1.5
-        # angular_velocity = self.last_angular_velocity * 0.9028 * 0.5 + torque_z * 0.0051 * 7.5
-        
         cmd_msg = Twist()
-        cmd_msg.linear.x = np.clip(linear_velocity, -MAX_LINEAR_VELOCITY, MAX_LINEAR_VELOCITY)
-        cmd_msg.angular.z = np.clip(angular_velocity, -MAX_ANGULAR_VELOCITY, MAX_ANGULAR_VELOCITY)
-        rospy.loginfo("v, omega = ({:.2f},{:.2f})".format(cmd_msg.linear.x, cmd_msg.angular.z))
+        cmd_msg.angular.z = np.clip(self.last_w, -MAX_ANGULAR_VELOCITY, MAX_ANGULAR_VELOCITY)
+        cmd_msg.linear.x = np.clip(self.last_v, -MAX_LINEAR_VELOCITY, MAX_LINEAR_VELOCITY)
         self.pub_vel.publish(cmd_msg)
+
+        v_dot = force_y / total_mass - self.last_v * DAMPING_XY / total_mass - CONSTANT_FRACTION_XY
+        w_dot = torque_z / MOMENT_OF_INERTIA - self.last_w * DAMPING_THETA / MOMENT_OF_INERTIA - CONSTANT_FRACTION_THETA
+        next_w = np.clip(self.last_w + w_dot * dt, -MAX_ANGULAR_VELOCITY, MAX_ANGULAR_VELOCITY)
+        next_v = np.clip(self.last_v + v_dot * dt, -MAX_LINEAR_VELOCITY, MAX_LINEAR_VELOCITY)
+        
+        rospy.loginfo("v': {:.3f}, w': {:.3f}".format(self.last_v , self.last_w))
+        rospy.loginfo("v : {:.3f}, w : {:.3f}".format(odom_msg.twist.twist.linear.x , odom_msg.twist.twist.angular.z))
+        rospy.loginfo("")
         
         self.last_time = now_time
-        # self.last_linear_velocity = linear_velocity
-        # self.last_angular_velocity = angular_velocity
-
-        self.last_linear_velocity = odom_msg.twist.twist.linear.x
-        self.last_angular_velocity = odom_msg.twist.twist.angular.z
+        self.last_v = next_v
+        self.last_w = next_w
 
 
 if __name__ == '__main__':
---
+++ catkin_ws/src/sensors/dynpick_driver/src/pushing_action_simulation_node.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import math
+import time
+import copy
+import numpy as np
+
+import rospy
+from std_srvs.srv import Trigger, TriggerResponse
+from walker_msgs.srv import PushingActionTrigger, PushingActionTriggerResponse
+from geometry_msgs.msg import WrenchStamped, Vector3, Twist
+from nav_msgs.msg import Odometry
+
+# The period of single-leg contacting the ground
+SINGLE_GAIT_PERIOD = 5.0
+MAX_PUSHING_FORCE_VALUE = 18.0
+MIN_PUSHING_FORCE_VALUE = 6.0
+UP_SLOPE_TIME_RATIO = 1 / 5
+UP_SLOPE = (MAX_PUSHING_FORCE_VALUE - MIN_PUSHING_FORCE_VALUE) / (SINGLE_GAIT_PERIOD / 2 * UP_SLOPE_TIME_RATIO)
+DOWN_SLOPE = (MIN_PUSHING_FORCE_VALUE - MAX_PUSHING_FORCE_VALUE) / (SINGLE_GAIT_PERIOD / 2 * (1.0 - UP_SLOPE_TIME_RATIO))
+rospy.logwarn("UP_SLOPE:{:.2f}, DOWN_SLOPE:{:.2f}".format(UP_SLOPE, DOWN_SLOPE))
+
+current_slope = 0.0
+simulated_force_frequency = 0.0
+force_y_value = 0.0
+force_start_time = None
+force_duration = None
+pub_force = None
+
+# Force wave
+#
+#          /\    /\    /\    /\    /\
+#         /  \  /  \  /  \  /  \  /  \
+#        /    \/    \/    \/    \/    \
+#       /    
+#      /       <---------->
+# ____/      SINGLE_GAIT_PERIOD
+
+def trigger_cb(req):
+    global force_duration
+    global force_start_time
+    global current_slope
+
+    resp = PushingActionTriggerResponse()
+    if req.duration_in_seconds <= 0:
+        rospy.logerr("Invalid force duration is given: {}, skip this trigger event.".format(req.duration_in_seconds))
+        resp.success = False
+    else:        
+        force_duration = req.duration_in_seconds
+        force_start_time = rospy.Time().now()
+        current_slope = UP_SLOPE * 2
+        resp.success = True
+    return resp
+
+
+
+def timer_cb(event):
+    global current_slope
+    global force_y_value
+
+    force_msg = WrenchStamped()
+
+    if force_start_time is not None and (rospy.Time.now() - force_start_time).to_sec() <= force_duration:
+        if current_slope > 0 and force_y_value >= MAX_PUSHING_FORCE_VALUE:
+            current_slope = DOWN_SLOPE
+        elif current_slope < 0 and force_y_value <= MIN_PUSHING_FORCE_VALUE:
+            current_slope = UP_SLOPE
+        force_y_value += (current_slope / simulated_force_frequency)
+    else:
+        force_y_value = 0
+
+    force_msg.wrench.force.y = force_y_value
+    force_msg.header.frame_id = "force_sensor_link"
+    force_msg.header.stamp = rospy.Time().now()
+    pub_force.publish(force_msg)
+
+
+if __name__ == '__main__':
+    rospy.init_node('simulated_pushing_action_node', anonymous=False)
+    pub_force = rospy.Publisher("/walker/force_filtered", WrenchStamped, queue_size=1)
+    srv_trigger = rospy.Service('trig_pushing_action', PushingActionTrigger, trigger_cb)
+
+    simulated_force_frequency = 20
+    rospy.Timer(rospy.Duration(1.0 / simulated_force_frequency), timer_cb)
+    rospy.loginfo(rospy.get_name() + ' is ready.')
+    rospy.spin()
